66fcda9e784fe7eaa3b44015ca0ab40b0ee98520
princeivankent <princeivankentmtiburcio@gmail.com>
2019-07-19 17:40:10 +0800

    updated minimal test

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div id="app">
     <HelloWorld msg="Full Stack JS Developer"/>
-    <ul>
-      <li 
-        v-for="api_token in api_tokens" 
-        :key="api_token.id"
-      >
-        {{ api_token.token }}
-      </li>
-    </ul>
+    <div class="container">
+      <button class="btn btn-sm" @click="fetchTokens">Fetch API Tokens</button>
+      <button class="btn btn-sm" @click="clearTokens">Clear Tokens</button>
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Token</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(api_token, index) in api_tokens" :key="api_token.id">
+            <td>{{ index+1 }}</td>
+            <td>{{ api_token.token }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -26,8 +36,14 @@ export default {
       api_tokens: []
     }
   },
-  mounted () {
-    axios.get('http://localhost:3000/api/api_tokens').then(({data}) => this.api_tokens = data)
+  methods: {
+    fetchTokens () {
+      axios.get('http://localhost:3000/api/api_tokens').then(({data}) => this.api_tokens = data)
+    },
+
+    clearTokens () {
+      this.api_tokens = []
+    }
   }
 }
 </script>
